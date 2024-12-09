3df0d93fe0a55ddf3921db5bd02ec06fb766f3a9
OscarSystemDeveloper <oscj87@gmail.com>
2024-12-09 11:05:01 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TravelGuide
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
