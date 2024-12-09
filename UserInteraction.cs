b68a9815ebf43afaf3aa81827fb050aa245eb39d
Balen-Nore <Balen.nore4@gmail.com>
2024-12-09 13:38:55 +0100

    AddedUserInteraction

---
+++ UserInteraction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelGuide
+{
+    public class UserInteraction
+    {
+        public static List<string> GetUserInput()
+        {
+            Console.WriteLine("Enter cities to visit (comma-separated):");
+            var input = Console.ReadLine();
+            return input.Split(',').Select(c => c.Trim()).ToList();
+        }
+
+        public static void DisplayResults(TripResult result)
+        {
+            Console.WriteLine(result.Summarize());
+        }
+
+        public static void DisplayError(string message)
+        {
+            Console.WriteLine($"Error: {message}");
+        }
+    }
+}
