aaad8be8d2dd0de02dcd452ea23d8e058ec7cee2
OscarSystemDeveloper <oscj87@gmail.com>
2024-12-09 13:44:29 +0100

    Add Cities

---
+++ AddCities.cs
@@ -0,0 +1,19 @@
+﻿namespace TravelGuide
+{
+    public class City
+    {
+        public string Name { get; set; }
+        public List<Route> Connections { get; set; }
+
+        public City(string name, List<Route> connections)
+        {
+            Name = name;
+            Connections = connections;
+        }
+
+        public List<Route> GetConnections()
+        {
+            return Connections;
+        }
+    }
+}
